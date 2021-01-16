adaff284a8c90fc6d2a3c32efd6916275c70d028
William Li <liwifyc@gmail.com>
2021-01-16 02:50:56 -0500

    fix merge

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import '../styles/globals.css';
 
 import Navbar from '../components/Navbar';
@@ -13,10 +12,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
-=======
-import { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
->>>>>>> 3f86fdbd227955b84f3b1d22fd0199f59f3f1431
