09f49b207a2008842869b6e38c8994134eaa9776
Keenan Gugeler <me@kgugeler.ca>
2021-01-16 01:43:01 -0500

    Add salt

--- backend/cryptbox/database/models/users.py
+++ backend/cryptbox/database/models/users.py
@@ -9,6 +9,7 @@ class Users(db.Model):
 
     username = dbcol(dbstr(256), unique = True, nullable = False)
     password_hash = dbcol(dbbinary, nullable = False)
+    salt = dbcol(dbstr(16), nullable = False)
 
     encrypted_private_key = dbcol(dbstr(65536), nullable = False)
     public_key = dbcol(dbstr(65536), nullable = False)
