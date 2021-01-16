9bfbb46d197c4f706c4da76070aeb9fffc2ae03a
Keenan Gugeler <me@kgugeler.ca>
2021-01-16 00:38:57 -0500

    update backend structure

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
-from flask import Flask
+from cryptbox import app
 
 import cryptbox.server.routes
-
-app = Flask('cryptbox', root_path = "cryptbox")
 
 if __name__ == '__main__':
     app.run(debug = True)
