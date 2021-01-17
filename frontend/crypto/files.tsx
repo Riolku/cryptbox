2c03ecce8097eba7fd72a0a7f8cc3cd6ba99c7de
Keenan Gugeler <me@kgugeler.ca>
2021-01-17 01:24:35 -0500

    add encryptRawContent

--- frontend/crypto/files.tsx
+++ frontend/crypto/files.tsx
@@ -18,13 +18,17 @@ async function decryptContent(requestData, master_key, iv) {
 async function encryptContent(data, key, iv) {
   let encoded_data = fromStringToBytes(data);
 
+  return encryptRawContent(raw_data, key, iv);
+}
+
+async function encryptRawContent(raw_data, key, iv) {
   return window.crypto.subtle.encrypt(
     {
       name : "AES-GCM",
       iv : iv
     },
     key,
-    encoded_data
+    raw_data
   );
 }
 
